016295630733ed27f81533b5d932c258c1f83f16
Anthony Gordon <anthony.gordon@shopify.com>
2019-04-11 22:21:41 +1200

    got word to stay inside tab

--- ext/src/inject/inject-tabs.js
+++ ext/src/inject/inject-tabs.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-  var $tabs = $(".template-editor-tab-filename");
-  var yas = "<span class='yas-container'></span>";
-  $tabs.append(yas);
+  console.log("working on load");
+  $(document).click(function() {
+    var $tabs = $(".template-editor-tab-filename");
+    var $yas = "<span class='yas-container'></span>";
+    var $yasContainer = $(".yas-container");
+    $yasContainer.remove();
+    $tabs.append($yas);
+  });
   $(document).click(function() {
     var $tabsInnnerContainer = $(".yas-container");
     var yasInner = "YAS";
     $tabsInnnerContainer.empty().append(yasInner);
+    console.log("working on click");
   });
 });
